483951ed509a6deb81dfb6991a4a370e6a66db2f
Biben <kasper_322@mail.ru>
2025-10-13 18:55:10 +0300

    commit v1.0.1 draft 4

--- backend/src/ydbase.py
+++ backend/src/ydbase.py
@@ -1,22 +1,60 @@
-import asyncio
-import sqlalchemy
-import ydb
-from config_reader import config
-import base64
+import time
+import jwt
+import json
 
+import yandexcloud
+from yandex.cloud.iam.v1.iam_token_service_pb2 import (CreateIamTokenRequest)
+from yandex.cloud.iam.v1.iam_token_service_pb2_grpc import IamTokenServiceStub
 
-def initializator(endpoint, database):
-    with ydb.Driver(
-        endpoint=endpoint,
-        database=database,
-        credentials=ydb.iam.ServiceAccountCredentials.from_file('../authorized_key.json')
-    ) as driver:
-        try:
-            driver.wait(timeout=5)
-        except TimeoutError:
-            print("Connect failed to YDB")
-            print("Last reported errors by discovery:")
-            print(driver.discovery_debug_details())
-            exit(1)
+key_path = '../authorized_key_4.json'
 
-initializator(endpoint=config.ydb_endpoint, database=config.ydb_database)
+# Чтение закрытого ключа из JSON-файла
+with open(key_path, 'r') as f:
+  obj = f.read() 
+  obj = json.loads(obj)
+  private_key = obj['private_key']
+  key_id = obj['id']
+  service_account_id = obj['service_account_id']
+
+sa_key = {
+    "id": key_id,
+    "service_account_id": service_account_id,
+    "private_key": private_key
+}
+
+def create_jwt():
+    now = int(time.time())
+    payload = {
+            'aud': 'https://iam.api.cloud.yandex.net/iam/v1/tokens',
+            'iss': service_account_id,
+            'iat': now,
+            'exp': now + 3600
+        }
+
+    # Формирование JWT.
+    encoded_token = jwt.encode(
+        payload,
+        private_key,
+        algorithm='PS256',
+        headers={'kid': key_id}
+    )
+
+    print(encoded_token)
+
+    return encoded_token
+
+def create_iam_token():
+  jwt = create_jwt()
+  
+  sdk = yandexcloud.SDK(service_account_key=sa_key, endpoint="api.yandexcloud.kz")
+  iam_service = sdk.client(IamTokenServiceStub)
+  iam_token = iam_service.Create(
+      CreateIamTokenRequest(jwt=jwt)
+  )
+  
+  print("Your iam token:")
+  print(iam_token.iam_token)
+
+  return iam_token.iam_token
+
+create_iam_token()
